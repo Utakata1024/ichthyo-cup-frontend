8417647dfe031ab56e59bcf3395ed496b33bafcc
Utakata1024 <tty1024kit@gmail.com>
2025-09-12 21:41:16 +0900

    ログインボタンにルート追加

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -4,7 +4,7 @@ export default function TopPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
             <h1 className="text-4xl font-bold mb-8">MusiChat</h1>
-            <Link href=""> {/* Spotify認証へのルート */}
+            <Link href="/api/auth/login">
                 <button className="bg-transparent text-purple-400 font-bold py-2 px-4 rounded-full border border-purple-400 shadow-lg hover:bg-purple-400 hover:text-gray-900 transition-colors">
                     ログイン
                 </button>
